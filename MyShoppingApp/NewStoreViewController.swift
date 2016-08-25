6aa7a75ed2b664ed7f44b89d3a42b9a4cfc17fa7
Jeff Norton <jeff12ntn@gmail.com>
2016-08-25 11:23:05 -0600

    Add select image functionality to new store view

--- MyShoppingApp/NewStoreViewController.swift
+++ MyShoppingApp/NewStoreViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import SafariServices
 
-class NewStoreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class NewStoreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     //==================================================
     // MARK: - Stored Properties
     //==================================================
     
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var selectImageButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var storeCategoriesTableView: UITableView!
     
@@ -66,5 +68,102 @@ class NewStoreViewController: UIViewController, UITableViewDataSource, UITableVi
         
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func saveButtonTapped(sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func selectImageButtonTapped(sender: UIButton) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let imageActionSheet = UIAlertController(title: "Choose an image source", message: nil, preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .Default) { (_) in
+            
+            imagePicker.sourceType = .PhotoLibrary
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+        let savedPhotosAction = UIAlertAction(title: "Saved Photos", style: .Default) { (_) in
+            
+            imagePicker.sourceType = .SavedPhotosAlbum
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .Default) { (_) in
+            
+            imagePicker.sourceType = .Camera
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+        let browserAction = UIAlertAction(title: "Browser", style: .Default) { (_) in
+            
+            guard let googleImagesUrl = NSURL(string: "https://www.google.com/imghp?gws_rd=ssl") else { return }
+            
+            let safariViewController = SFSafariViewController(URL: googleImagesUrl)
+            
+            self.presentViewController(safariViewController, animated: true, completion: nil)
+        }
+        
+        imageActionSheet.addAction(cancelAction)
+        imageActionSheet.addAction(browserAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            
+            imageActionSheet.addAction(photoLibraryAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.SavedPhotosAlbum) {
+            
+            imageActionSheet.addAction(savedPhotosAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            
+            imageActionSheet.addAction(cameraAction)
+        }
+        
+        self.presentViewController(imageActionSheet, animated: true, completion: nil)
+    }
+    
+    //==================================================
+    // MARK: - Method(s)
+    //==================================================
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        
+        selectImageButton.titleLabel?.text = ""
+        imageView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
